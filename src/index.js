c72b4de918c2e783bb5f3cafd5b2542fe40d2a4f
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-07 15:41:53 -0600

    created a contact page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import _ from 'lodash';
 import { homePage } from './page-load';
 import './style.css';
 import {menuPage} from './menu';
+import {contactPage} from './contact';
 
-menuPage();
+contactPage();
 
 
