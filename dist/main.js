b678318407aecaea0bee8bce76b08ea7d7f46d5b
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-07 11:18:52 -0600

    I am trying to render the DOM elements from an js module but currently I am unable to grab the content id from the html file

--- dist/main.js
+++ dist/main.js
@@ -7,15 +7,16 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log('Hello World');\n\n//# sourceURL=webpack://project-restuarant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _page_load__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./page-load */ \"./src/page-load.js\");\n\n\n(0,_page_load__WEBPACK_IMPORTED_MODULE_0__.pageLoad)();\n\n//# sourceURL=webpack://project-restuarant-page/./src/index.js?");
 
 /***/ }),
 
@@ -23,21 +24,74 @@ eval("console.log('Hello World');\n\n//# sourceURL=webpack://project-restuarant-
 /*!**************************!*\
   !*** ./src/page-load.js ***!
   \**************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("// grab the content id from the index.html file\nconst contentId = document.getElementById('content');\n\n\n\n//# sourceURL=webpack://project-restuarant-page/./src/page-load.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"pageLoad\": () => (/* binding */ pageLoad)\n/* harmony export */ });\n// create a constant for the content id from the html located in the dist folder\n\nconst content = document.getElementById('content');\n\n\n\n\n// create a function to create the elements for the page\n\nconst pageLoad = () => {\n    // create a div for the header\n    const header = document.createElement('div');\n    header.setAttribute('id', 'header');\n    content.appendChild(header);\n    // create a div for the main\n    const main = document.createElement('div');\n    main.setAttribute('id', 'main');\n    content.appendChild(main);\n    // create a div for the footer\n    const footer = document.createElement('div');\n    footer.setAttribute('id', 'footer');\n    content.appendChild(footer);\n    }\n\n// export the function\n\n\n\n\n\n//# sourceURL=webpack://project-restuarant-page/./src/page-load.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	__webpack_modules__["./src/index.js"]();
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/page-load.js"]();
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/page-load.js");
 /******/ 	
 /******/ })()
 ;
--- src/page-load.js
+++ src/page-load.js
@@ -1,3 +1,28 @@
-// grab the content id from the index.html file
-const contentId = document.getElementById('content');
+// create a constant for the content id from the html located in the dist folder
+
+const content = document.getElementById('content');
+
+
+
+
+// create a function to create the elements for the page
+
+const pageLoad = () => {
+    // create a div for the header
+    const header = document.createElement('div');
+    header.setAttribute('id', 'header');
+    content.appendChild(header);
+    // create a div for the main
+    const main = document.createElement('div');
+    main.setAttribute('id', 'main');
+    content.appendChild(main);
+    // create a div for the footer
+    const footer = document.createElement('div');
+    footer.setAttribute('id', 'footer');
+    content.appendChild(footer);
+    }
+
+// export the function
+
+export { pageLoad };
 
