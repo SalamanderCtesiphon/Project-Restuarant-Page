2ff529f1c8fbfbe22d039ee0bf9c45863fbfd249
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-07 15:14:42 -0600

    cleaned the homepage up

--- src/page-load.js
+++ src/page-load.js
@@ -38,7 +38,8 @@ function createMain () {
     image.alt = 'Restaurant';
 
     const description = document.createElement('p');
-    description.textContent = 'Local food, local people, local atmosphere';
+    description.textContent = 'Local food, local people, local atmosphere. lorem ipsum laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    description.classList.add('description');
     main.appendChild(title);
     main.appendChild(image);
     main.appendChild(description);
