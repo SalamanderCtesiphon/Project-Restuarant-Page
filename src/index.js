c7f6bade4bf2ec3274f91dcbde6f94e4e75f417b
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-09 13:23:42 -0600

    still working

--- src/index.js
+++ src/index.js
@@ -12,40 +12,4 @@ const tabOne = document.querySelector('.tabOne');
 const tabTwo = document.querySelector('.tabTwo');
 const tabThree = document.querySelector('.tabThree');
 
-
-class PageState {
-    constructor() {
-        this.page = homePage();
-    }
-
-    get currentPage() {
-        return this.page;
-    }
-
-    set currentPage(page) {
-        tabOne.addEventListener('click', () => {
-            this.page = homePage();
-            return homePage();
-        }
-        );
-        tabTwo.addEventListener('click', () => {
-            this.page = menuPage();
-            return menuPage();
-        }
-        );
-        tabThree.addEventListener('click', () => {
-            this.page = contactPage();
-            return contactPage();
-        }
-        );
-
-        this.page = page;
-
-    }
-
-    
-}
-
-// Instantiate pageState
-const page = new PageState();
-
+homePage();
