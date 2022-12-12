93829dc799624e1cd587d94dd12f8bdb0550a9ff
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-12 09:55:37 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-import _, { initial } from 'lodash';
 import { homePage } from './page-load';
 import './style.css';
 import {menuPage} from './menu';
 import {contactPage} from './contact';
-
-
-
 const content = document.getElementById('content');
 
 init();
-
 function init () {
     return homePage();
 }
 
-
-
 const tabOne = document.querySelector('.tabOne');
 const tabTwo = document.querySelector('.tabTwo');
 const tabThree = document.querySelector('.tabThree');
-const tabOneValue = tabOne.getAttribute('value');
-const tabTwoValue = tabTwo.getAttribute('value');
-const tabThreeValue = tabThree.getAttribute('value');
+
+function getClick () {
+    tabOne.addEventListener('click', () => {
+        content.innerHTML = '';
+        init();
+    }
+    );
+    tabTwo.addEventListener('click', () => {
+        content.innerHTML = '';
+        menuPage();
+    }
+    );
+    tabThree.addEventListener('click', () => {
+        content.innerHTML = '';
+        contactPage();
+    }
+    );    
+}
+
+getClick();
 
 
 
